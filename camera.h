4e4e823ea1a3f7ef9f9bb35b608b8101b98de83a
Zoe <2625792753@qq.com>
2023-09-27 23:34:07 +0800

    portable camera

--- camera.h
+++ camera.h
@@ -13,6 +13,11 @@ public:
     int samplers_per_pixel = 10;      // Amount of samplers for each pixel
     double ray_gen_probability = 0.6; // Probability of ray generation. (Instead of using max depth, let's try Russian Roulette!)
 
+    double vfov = 90;                   // Vertical field of view
+    point3 lookfrom = point3(0, 0, -1);  // Point where camera is looking from
+    point3 lookat = point3(0, 0, 0);   // Point where camera is looking at
+    vec3 vup = vec3(0, 1, 0);           // Absolute up direction (world space)
+
     // Rendering
     void render(const hittable_list /* don't get it, but it works, and it won't work when get const hittable here*/ &world)
     {
@@ -79,29 +84,37 @@ private:
     point3 pixel00_pos; // Position of pixel (0, 0)
     vec3 pixel_delta_u; // Offset to pixel to the right
     vec3 pixel_delta_v; // Offset to pixel below
+    vec3 u, v, w;       // Camera frame basis vectors
 
     void initialize()
     {
         image_height = static_cast<int>(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
 
-        center = point3(0, 0, 0);
+        center = lookfrom;
 
         // Viewport
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        auto focal_length = (lookfrom - lookat).length();
+        auto theta = degree2radius(vfov);
+        auto h = tan(theta / 2);
+        auto viewport_height = 2.0 * h * focal_length;
         auto viewport_width = viewport_height * static_cast<double>(image_width) / image_height;
 
+        // u, v, w Unit basis vectors for camera coordinate frame
+        w = normalize(lookfrom - lookat);
+        u = normalize(cross(vup, w));
+        v = cross(w, u);
+
         //  Viewport delta offsets
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+        auto viewport_u = viewport_width * u;   // Vector across viewport horizontal edge
+        auto viewport_v = viewport_height * -v; // Vector down viewport vertical edge
 
         // Viewport delta pixel_offsets
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
         // Position of pixel (0, 0)
-        auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+        auto viewport_upper_left = center - w * focal_length - viewport_u / 2 - viewport_v / 2;
         pixel00_pos = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 };
--- main.cpp
+++ main.cpp
@@ -9,8 +9,7 @@ int main()
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    // auto material_left =   make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.6));
     auto material_left = make_shared<dielectric>(1.5);
     auto material_right =  make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
@@ -28,7 +27,13 @@ int main()
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 800;
     cam.samplers_per_pixel = 256;
-    cam.ray_gen_probability = 0.96;
+    cam.ray_gen_probability = 0.9;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(-2, 2, 1);
+    cam.lookat = point3(0, 0, -1);
+    cam.vup = vec3(0, 1, 0);
+
     clog << "Samplers: " << cam.samplers_per_pixel << '\n';
 
     cam.render(world);
