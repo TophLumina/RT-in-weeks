b96b7aba02582d1d35d1956dba854b404fe88e62
Zoe <2625792753@qq.com>
2023-10-11 14:08:53 +0800

    Andrew Kensler's method

--- aabb.h
+++ aabb.h
@@ -29,15 +29,24 @@ public:
     }
 
     bool hit(const ray& r, interval ray_t) const
+    // Andrew Kensler's optimized method
     {
         for (int a = 0; a < 3; ++a)
         {
-            auto t0 = fmin(axis(a).min - r.origin()[a] / r.direction()[a], axis(a).max - r.origin()[a] / r.direction()[a]);
-            auto t1 = fmax(axis(a).min - r.origin()[a] / r.direction()[a], axis(a).max - r.origin()[a] / r.direction()[a]);
+            auto invD = 1.0 / r.direction()[a];
+            auto orig = r.origin()[a];
 
-            ray_t.min = fmax(ray_t.min, t0);
-            ray_t.max = fmin(ray_t.max, t1);
+            double t0 = (axis(a).min - orig) * invD;
+            double t1 = (axis(a).max - orig) * invD;
 
+            if (invD < 0)
+                std::swap(t0, t1);
+            
+            if (t0 > ray_t.min)
+                ray_t.min = t0;
+            if (t1 < ray_t.max)
+                ray_t.max = t1;
+            
             if (ray_t.max <= ray_t.min)
                 return false;
         }
